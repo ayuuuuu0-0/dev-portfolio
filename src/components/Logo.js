6eeb403338e8f1fc5a6a697b4f667b1db4a179ae
Ayush Ranjan <ayush.rajan@miracuves.com>
2024-08-29 23:58:57 +0530

    new commit

---
+++ src/components/Logo.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import { motion } from "framer-motion";
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center mt-2">
+      <MotionLink
+        href="/"
+        className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
+        whileHover={{
+          scale: [1, 1.1, 1, 1.1, 1],
+          rotate: [0, 0, 360, 360, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          backgroundColor: ["#000000", "#51585B", "#000000"],
+          boxShadow: [
+            "0px 0px 0px rgba(0, 0, 0, 0)",
+            "0px 0px 10px rgba(81, 88, 91, 0.5)",
+            "0px 0px 20px rgba(81, 88, 91, 1)",
+            "0px 0px 10px rgba(81, 88, 91, 0.5)",
+            "0px 0px 0px rgba(0, 0, 0, 0)",
+          ],
+          transition: {
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+          },
+        }}
+      >
+        AR
+      </MotionLink>
+    </div>
+  );
+};
+
+export default Logo;
